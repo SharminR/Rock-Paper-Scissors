e71a6f62bfcf12be9e224b29593014456e525d89
Mojjammil Khandker <mojjammil@gmail.com>
2018-02-05 23:57:14 +0200

    Browser crashing

--- rock, paper, scissors.js
+++ rock, paper, scissors.js
@@ -1,49 +1,53 @@
 var userChoice = document.getElementById("userChoice").value;
-var computerChoice = Math.random();
+console.log(userChoice);
 
 function AiChoice () {
-if (computerChoice <0.34){
-    document.getElementById("computerChoice").innerHTML = "rock";
-    computerChoice = "rock";
-} else if(computerChoice <=0.67){
-    document.getElementById("computerChoice").innerHTML = "paper";
-    computerChoice = "paper";
-} else {
-    document.getElementById("computerChoice").innerHTML = "scissors";
-    computerChoice = "scissors";
-}
-return computerChoice;
+  var computerChoice = Math.random();
+  if (computerChoice <0.34){
+    document.getElementById("computerChoice").innerHTML = "Rock";
+    //computerChoice = "Rock";
+  } else if(computerChoice <=0.67){
+    document.getElementById("computerChoice").innerHTML = "Paper";
+    //computerChoice = "Paper";
+  } else {
+    document.getElementById("computerChoice").innerHTML = "Scissors";
+    //computerChoice = "Scissors";
+  }
+  //return computerChoice;
 }
 
-function compare(choice1,choice2){
-    if(choice1 == choice2){
-        return "The result is a tie!";
+function compare(){
+  var AiMove = AiChoice();
+  console.log(AiMove);
+
+    if (AiMove == userChoice) {
+        document.getElementById("result").innerHTML = "The result is a tie!";
     }
-    if(choice1 == "rock"){
-        if(choice2 == "scissors"){
-            return "rock wins";
+    
+    while (userChoice == "Rock") {
+        if (AiMove == "Scissors") {
+            document.getElementById("result").innerHTML = "You win";
         }
-        else{
-            return "paper wins";
-        }
-    }
-    if(choice1 == "paper"){
-        if(choice2 == "rock"){
-            return "paper wins";
-        }
-        else{
-            return "scissors wins";
+        else {
+            document.getElementById("result").innerHTML = "AI wins";
         }
     }
-    if(choice1 == "scissors"){
-        if(choice2 == "rock"){
-            return "rock wins";
+
+    while (userChoice == "Paper") {
+        if (AiMove == "Scissors") {
+            document.getElementById("result").innerHTML = "You win";
         }
-        else{
-            return "scissors wins";
+        else {
+            document.getElementById("result").innerHTML = "AI wins";
+        }
+    }
+
+    while (userChoice == "Scissors") {
+        if (AiMove == "Paper") {
+            document.getElementById("result").innerHTML = "You win";
+        }
+        else {
+            document.getElementById("result").innerHTML = "AI wins";
         }
     }
 }
-console.log(compare(userChoice,computerChoice));
-
-document.getElementById('result').innerHTML = userChoice, computerChoice;
